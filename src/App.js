8c8bf973f73259d5acaccc333264f628579ff1c2
Paskuvan <majopaskuvan@gmail.com>
2021-11-15 10:58:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem  from "./components/ExpenseItem";  
+
+function App() {
+  return (
+  <div>
+      <h1>Lets started </h1>
+      <ExpenseItem></ExpenseItem>
+  </div>
+    
+
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+	return	(
+	<div>
+		<div>November 15th, 2021</div>
+	<div>
+		<h2>Insurance</h2>
+		<div>$294.66</div>
+	</div>
+	</div>
+	);
+}
+
+export default ExpenseItem;
