0b82815045d2fa2826bef5db6c5ed8bb4c2e392c
Paskuvan <majopaskuvan@gmail.com>
2021-11-18 15:39:37 -0300

    expensesfilter

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
 
   const addExpenseHandler = expense => {
     console.log('In App.js');
-    console.log(expense)
+    console.log(expense);
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ExpenseItem from './ExpenseItem';  
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
-function Expenses(props) {
-    return (
-      <Card className="expenses">
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
@@ -27,7 +37,8 @@ function Expenses(props) {
           date={props.items[3].date}
         />
       </Card>
-    );
-  }
-  
-  export default Expenses;
+    </div>
+  );
+};
+
+export default Expenses;
